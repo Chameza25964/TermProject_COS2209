519448d4130aa9859627e899895cdb1b02986a6d
TheNight LordChame <Chameza007@hotmail.com>
2019-04-16 21:59:57 +0700

    Hello

--- OCR_CsharpAPP/MainFunctional.cs
+++ OCR_CsharpAPP/MainFunctional.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Google.Apis.Auth.OAuth2;
 using Google.Cloud.Vision.V1;
